338b3902380eba09b7a90f5d1bad0e2deaaaba02
tedj <tedj.meabiou@gmail.com>
2016-08-12 11:59:15 +0100

    update the web_service runner

--- run_web_service.py
+++ run_web_service.py
@@ -1,4 +1,4 @@
 from population_estimator import app
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=80, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
